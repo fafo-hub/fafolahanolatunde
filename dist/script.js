84d27b039af6806d613a98b4ff9a4950f629144e
Fafooo <62049344+fafo-hub@users.noreply.github.com>
2021-03-18 08:29:14 -0700

    Recent commit

--- dist/script.js
+++ dist/script.js
@@ -12,19 +12,18 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
-let home_header = document.querySelector('#home');
+
 //Select Initial State of Menu
 
 let showMenu = true;
 menuBtn.addEventListener('click', toggleMenu);
-//console.log(menuBtn);
+
 function toggleMenu() {
     if(!showMenu) {
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
-        home_header.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
         showMenu = true;
     }
@@ -33,7 +32,6 @@ function toggleMenu() {
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
-        home_header.classList.add('show');
         navItems.forEach(item => item.classList.remove('show'));
 
         
